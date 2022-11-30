4ea32b1431161a478bc669046e5e30185c878b66
gggeek <giunta.gaetano@gmail.com>
2022-11-30 11:27:33 +0000

    avoid lsb_release

--- docker/setup/install_packages.sh
+++ docker/setup/install_packages.sh
@@ -7,6 +7,6 @@ set -e
 echo "Installing base software packages..."
 
 DEBIAN_FRONTEND=noninteractive apt-get install -y \
-    curl git lsb-release sudo unzip
+    curl git sudo unzip
 
 echo "Done installing base software packages"
